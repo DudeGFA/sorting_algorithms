23c9eac889aad150f852cb5fcf241c291d325b1d
DudeGFA <favouridowuagida0@gmail.com>
2022-08-16 13:36:18 +0100

    update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,9 +1,18 @@
 #include "sort.h"
 
+/**
+ * return_max - returns max value in an array
+ * 
+ * @arr: array 
+ * @n: size of array 
+ * Return: int 
+ */
 int return_max(int arr[], int n)
 {
     int max_val = arr[0];
-    for (int i = 1; i < n; i++)
+    int i;
+
+    for (i = 1; i < n; i++)
     {
         if (arr[i] > max_val)
             max_val = arr[i];
@@ -11,9 +20,18 @@ int return_max(int arr[], int n)
     return max_val;
 }
 
+/**
+ * countsort - sorts an array using
+ * countsort algorithm
+ * 
+ * @array: array to be sorted 
+ * @n: size of array
+ * @place_val: place value sorting depends on
+ * Returns: nothing
+ */
 void countsort(int *array, int n, int place_val)
 {
-    int output[n];
+    int *output = malloc(sizeof(int) * n);
     int i;
     int count[10] = {0};
 
@@ -26,8 +44,20 @@ void countsort(int *array, int n, int place_val)
         output[count[(array[i] / place_val) % 10] - 1] = array[i];
         count[(array[i] / place_val) % 10]--;
     }
+    for (i = 0; i < n; i++)
+    {
+        array[i] = output[i];
+    }
     print_array(array, n);
 }
+/**
+ * radix_sort - sorts an array using radix
+ * algorithm
+ * 
+ * @array: array to be sorted
+ * @size: size of array
+ * Returns: nothing
+ */
 void radix_sort(int *array, size_t size)
 {
     int max_val = return_max(array, size);
--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define _SORT_H_
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
